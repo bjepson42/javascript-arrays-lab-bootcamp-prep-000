c4676d1063d535ceca1dc05b2dbd793dbd0dbb9d
Beau Jepson <beau.jepson@jepsonconsulting.com>
2018-11-06 15:46:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 
 // Add your functions and code here
-destructivelyAppendKitten(arr, foo){
-
-  return [arr..., foo]
+destructivelyAppendKitten(foo){
+  kittens.push(foo)
 }
+
+
